cb3b548a6fe110d3280b87c0e46f5793b5e61f01
EdwinMindcraft <edwin.mindcraft@gmail.com>
2022-03-14 13:22:21 +0100

    HudRender texture fix

--- src/main/java/io/github/apace100/apoli/util/HudRender.java
+++ src/main/java/io/github/apace100/apoli/util/HudRender.java
@@ -13,15 +13,17 @@ import java.util.Optional;
 public record HudRender(boolean shouldRender, int barIndex, ResourceLocation spriteLocation,
 						ConfiguredEntityCondition<?, ?> condition, boolean inverted) {
 
+	private static final ResourceLocation DEFAULT_SPRITE = new ResourceLocation("origins", "textures/gui/resource_bar.png");
+
 	public static final Codec<HudRender> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			CalioCodecHelper.optionalField(Codec.BOOL, "should_render", true).forGetter(HudRender::shouldRender),
 			CalioCodecHelper.optionalField(Codec.INT, "bar_index", 0).forGetter(HudRender::barIndex),
-			CalioCodecHelper.optionalField(ResourceLocation.CODEC, "sprite_location", Apoli.identifier("textures/gui/resource_bar.png")).forGetter(HudRender::spriteLocation),
+			CalioCodecHelper.optionalField(ResourceLocation.CODEC, "sprite_location", DEFAULT_SPRITE).forGetter(HudRender::spriteLocation),
 			CalioCodecHelper.optionalField(ConfiguredEntityCondition.CODEC, "condition").forGetter(x -> Optional.ofNullable(x.condition())),
             CalioCodecHelper.optionalField(Codec.BOOL, "inverted", false).forGetter(HudRender::inverted)
 	).apply(instance, (t1, t2, t3, t4, t5) -> new HudRender(t1, t2, t3, t4.orElse(null), t5)));
 
-	public static final HudRender DONT_RENDER = new HudRender(false, 0, Apoli.identifier("textures/gui/resource_bar.png"), null, false);
+	public static final HudRender DONT_RENDER = new HudRender(false, 0, DEFAULT_SPRITE, null, false);
 
 	public ResourceLocation getSpriteLocation() {
 		return this.spriteLocation();
