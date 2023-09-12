7ea5bb53e37418f40c9873b521107fbd4fc7a5e7
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-09-12 19:20:05 +1000

    Apoli: Update NETWORK_VERSION value.

--- src/main/java/io/github/edwinmindcraft/apoli/common/ApoliCommon.java
+++ src/main/java/io/github/edwinmindcraft/apoli/common/ApoliCommon.java
@@ -21,7 +21,7 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class ApoliCommon {
-	public static final String NETWORK_VERSION = "1.0";
+	public static final String NETWORK_VERSION = "1.1";
 
 	public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(Apoli.identifier("channel"))
 			.networkProtocolVersion(() -> NETWORK_VERSION)
